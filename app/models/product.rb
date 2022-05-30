c84089db63dc35cec3c30de0bf2a812328a66e57
Luiz Affonso <luiz.m.affonso01@gmail.com>
2022-05-29 23:48:05 -0300

    Add product with optional user

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,16 +6,12 @@
 #  title      :string
 #  price      :decimal(, )
 #  published  :boolean
-#  user_id    :bigint           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#
-# Indexes
-#
-#  index_products_on_user_id  (user_id)
+#  user_id    :bigint
 #
 class Product < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   validates :title, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
