290087d77532c66b1ba586d8ecf0a609ce8ea1d5
Luiz Affonso <luiz.m.affonso01@gmail.com>
2022-05-29 23:49:19 -0300

    Update product factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -6,19 +6,14 @@
 #  title      :string
 #  price      :decimal(, )
 #  published  :boolean
-#  user_id    :bigint           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#
-# Indexes
-#
-#  index_products_on_user_id  (user_id)
+#  user_id    :bigint
 #
 FactoryBot.define do
   factory :product do
     title { "MyString" }
     price { "9.99" }
     published { false }
-    user { nil }
   end
 end
