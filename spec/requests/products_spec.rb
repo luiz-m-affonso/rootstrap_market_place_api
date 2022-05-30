6bf6bab500b661ec4dc0a47a019b8d2cac3676b0
Luiz Affonso <luiz.m.affonso01@gmail.com>
2022-05-29 23:59:06 -0300

    Update product spec

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -6,13 +6,9 @@
 #  title      :string
 #  price      :decimal(, )
 #  published  :boolean
-#  user_id    :bigint           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#
-# Indexes
-#
-#  index_products_on_user_id  (user_id)
+#  user_id    :bigint
 #
 require 'rails_helper'
 
